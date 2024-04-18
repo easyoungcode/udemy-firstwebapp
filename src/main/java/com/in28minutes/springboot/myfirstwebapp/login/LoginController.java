a706edb13bb255b68c549aa9d5247bf919af7452
easyoungcode <gchun910@gmail.com>
2024-04-18 11:24:32 +0900

    Method GET vs POST

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,18 +1,33 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
     // Model
     // Get에 param 형태로 parameter를 넘겨주는 중
-    @RequestMapping("login")
-    public String gotoLogin(@RequestParam String name) {
-        System.out.println("name " + name);
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String gotoLogin() {
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String gotoWelcome(@RequestParam String name,
+                              @RequestParam String password,
+                              ModelMap model) {
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+    Welcome! <br>
+    Name : ${name} <br>
+    Password : ${password}
+</body>
+</html>
