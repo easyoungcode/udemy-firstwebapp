78d0384d98e7b78cf969f686bb52bc2de8acfe22
easyoungcode <gchun910@gmail.com>
2024-04-23 09:38:34 +0900

    SecurityFilterChain 설정

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/SpringSecurityConfiguration.java
@@ -1,13 +1,17 @@
 package com.in28minutes.springboot.myfirstwebapp;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -38,5 +42,23 @@ public class SpringSecurityConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    //All URLs are protected
+    //A login form is shown for unauthorized requests
+    //CSRF disable
+    //Frames (프레임 허용해줘야 함)
+
+    /** 처음에 login page가 뜨는 것 -> securityFilterChain이 함. */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        return http.build();
     }
 }
