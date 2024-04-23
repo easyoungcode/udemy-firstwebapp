bb3b35fea613dceedbb8433324f5c8e5c589ae32
easyoungcode <gchun910@gmail.com>
2024-04-23 09:04:15 +0900

    UserDetails로 사용자 확인

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/SpringSecurityConfiguration.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/SpringSecurityConfiguration.java
@@ -16,16 +16,23 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+
+        UserDetails userDetails1 = createNewUser("ecode", "1111");
+        UserDetails userDetails2 = createNewUser("hi", "1212");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder
             = input -> passwordEncoder().encode(input);
         UserDetails userDetails = User.builder()
             .passwordEncoder(passwordEncoder)
-            .username("ecode")
-            .password("1111")
+            .username(username)
+            .password(password)
             .roles("ADMIN", "USER")
             .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        return userDetails;
     }
 
     @Bean
--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -15,15 +15,15 @@ public class TodoService {
     private static int todosCount = 0;
 
     // 정적변수 초기화
-    static {
-        todos.add(new Todo(++todosCount, "ecode", "Learn AWS"
-            , LocalDate.now().plusYears(1), false));
-        todos.add(new Todo(++todosCount, "ecode", "Learn DevOps"
-            , LocalDate.now().plusYears(2), false));
-        todos.add(new Todo(++todosCount, "ecode", "Learn Spring"
-            , LocalDate.now().plusYears(3), false));
-
-    }
+//    static {
+//        todos.add(new Todo(++todosCount, "ecode", "Learn AWS"
+//            , LocalDate.now().plusYears(1), false));
+//        todos.add(new Todo(++todosCount, "ecode", "Learn DevOps"
+//            , LocalDate.now().plusYears(2), false));
+//        todos.add(new Todo(++todosCount, "ecode", "Learn Spring"
+//            , LocalDate.now().plusYears(3), false));
+//
+//    }
 
     public List<Todo> findByUserName(String userName) {
         Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(userName);
